6b9b7bb1d2afee1d5a4ba3ad9fe36096c620144d
tonylu0 <tonylu123@hotmail.com>
2018-12-06 04:03:35 -0600

    Updating SRLatch

--- Sources/register.v
+++ Sources/register.v
@@ -2,24 +2,26 @@
 // srlatch: 1 bit sr latch 
 //
 // q      (output) - Current value of register
-// d      (output) - Opposite value of register
 // s      (set)    - Sets value to 1
+// r      (reset)  - Resets latch to 0
 //
-module srlatch(q, s);
+module srlatch(q, s, r);
 
     parameter 
         width = 8,
         reset_value = 0;
     
     output  [(width - 1):0] q;
-    input   [(width - 1):0] s;
+    input   [(width - 1):0] s, r;
 
-    wire    [(width - 1):0] r;
     wire    [(width - 1):0] d;
+    wire    [(width - 1):0] in, nreset;
 
-    assign  r = !s;
+    not     n(nreset, r);
+    and     a(in, s, nreset);
+
 
     nor     [(width - 1):0] up(q, r, d);
-    nor     [(width - 1):0] down(d, s, q);
+    nor     [(width - 1):0] down(d, in, q);
 
 endmodule // srlatch
